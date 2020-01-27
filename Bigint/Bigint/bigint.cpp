59fe0911ee94870f569fc99a5e8929335b98d833
Adnan Mohamed <amohame6@kent.edu>
2020-01-27 17:24:35 -0500

    Lab 1, Milstone 1

---
+++ Bigint/Bigint/bigint.cpp
@@ -0,0 +1,91 @@
+// This is the implementation file for the class bigint.
+// You could see the interface in bigint.h
+// @ Coded by: Adnan Mohamed
+
+#include<iostream>
+#include"bigint.hpp"
+using std::cout; using std::cin; using std::endl;
+
+
+	bigint::bigint(int number)
+	{
+		int index = 0;
+		length_ = 0;
+
+		// assign the given number to the array such that ones be stored in index 0,
+		// tens in index 1 and so on.
+		while (number > 0)
+		{
+			number_[index] = number % 10;
+			number /= 10;
+			++index;
+			++length_;
+		}
+
+		if (length_ == 0)
+			length_ = 1;
+		for (int i = index; i < CAPACITY; ++i)
+		{
+			number_[index] = 0;
+		}
+
+	}
+
+	bigint::bigint(const char number[])
+	{
+		length_ = std::strlen(number);
+		int index = length_ - 1;
+		while (index >= 0)
+		{
+			number_[length_ - index - 1] = digit_to_int(number[index]);
+			--index;
+		}
+		for (int i = length_; i < CAPACITY; ++i)
+		{
+			number_[i] = 0;
+		}
+	}
+
+	void bigint::debugPrint(std::ostream& outs) const
+	{
+		for (int i = CAPACITY- 1; i >= 0; --i)
+		{
+			outs << number_[i]<<"|";
+		}
+	}
+
+	int digit_to_int(char ch)
+	{
+		return static_cast<int>(ch) - static_cast<int>('0');
+	}
+
+	std::ostream& operator<<(std::ostream& outs, const bigint& number)
+	{
+		/*if (number.length_ == 1)
+		{
+			outs << number.;
+			return outs;
+		}*/
+		for (int i = number.length_ - 1; i >= 0; --i)
+		{
+			outs << number.number_[i];
+		}
+		return outs;
+	}
+
+	bool operator==(const bigint& number1, const bigint& number2)
+	{
+		// check their lengths if not the same return false
+		// else compare each number in the arrays
+		if (number1.length_ != number2.length_)
+			return false;
+		else
+		{
+			for (int i = 0; i < number1.length_; ++i)
+			{
+				if (number1.number_[i] != number2.number_[i])
+					return false;
+			}
+			return true;
+		}
+	}
---
+++ Bigint/Bigint/bigint.hpp
@@ -0,0 +1,62 @@
+// This is the interface file for the class Bigint.
+// Bigint allows storing larger numbers than int.
+//
+// @Author: Adnan Mohamed
+//
+// CONSTRUCTORS: 
+//	bigint(int number = 0)
+//	  Precondition: number >= 0
+//	  Postconditon: the bigint equals the given number (default- 0)
+//
+//	bigint(const char number[])
+//	  Precondition: the number presented as a string is valid non-negative number
+//	  Postcondition: the bigint equals the given number
+//
+// MEMBER FUNCTIONS:
+//	void debugPrint(std::ostream& outs) const
+//	  Postcondition: The number is printed
+//
+//	int get_length()const
+//	  Postcondition: Returns the how many digits in the number.
+//
+// NON-MEMBER FUNCTIONS:
+//	int digit_to_int(char ch)
+//	  Precondition: ch is a valid digit (0-9).
+//	  Postcondition: the integer that equals the character digit is returned.
+//
+//	std::ostream& operator<<(std::ostream& outs, const bigint& number)
+//	  Precondition: number is a number of type bigint (non-negative)
+//	  Postcondition: outputs the number to the stream.
+//
+//	bool operator==(const bigint& number1, const bigint& number2)
+//	  Postcondition: Returns true if the numbers are equal false otherwise.
+#ifndef BIGINT_H
+#define BIGINT_H
+#include<iostream>
+
+	const int CAPACITY = 400;
+	class bigint
+	{
+	public:
+		//CONSTRUCTORS
+		bigint(int number = 0);
+		bigint(const char number[]);
+
+		// MEMBER FUNCTIONS
+		void debugPrint(std::ostream& outs) const;
+
+		// Accessors
+		int get_length()const { return length_; }
+
+		// Friends
+		friend std::ostream& operator<<(std::ostream& outs, const bigint& number);
+		friend bool operator==(const bigint& number1, const bigint& number2);
+	private:
+		int number_[CAPACITY] ;
+		int length_; // stores the number of digits in the number
+	};
+
+	int digit_to_int(char ch);
+
+#endif // !BIGINT_H
+
