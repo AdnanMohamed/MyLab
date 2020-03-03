c56861b2d81292426ed2bb331fea5cc456a3d73e
Adnan Mohamed <amohame6@kent.edu>
2020-03-02 20:16:32 -0500

    tested (easy)

---
+++ Gift List/Gift List/gift_list.cpp
@@ -0,0 +1,186 @@
+// The implementation file for the class gift_list and a simple class Name
+// See interface file in gift_list.h
+//
+// @Author: Adnan Hashem Mohamed
+//
+// CLASS INVARIANT (gift_list):
+//
+
+#include"gift_list.h"
+#include<string>
+
+
+namespace gift_list_adnan {
+
+	void Name::format_name(std::string& name)
+	{
+		if (name.length() == 0)
+			return;
+		// Make all letters lower except the first.
+		name[0] = toupper(name[0]);
+		
+		for (int i = 1; i < name.length(); ++i)
+		{
+			name[i] = tolower(name[i]);
+		}
+	}
+
+	Name::Name(std::string first, std::string last, std::string middle)
+	{
+		format_name(first);
+		format_name(middle);
+		format_name(last);
+		first_ = first;
+		last_ = last;
+		middle_ = middle;
+	}
+
+	void Name::set_first(std::string first)
+	{
+		format_name(first);
+		first_ = first;
+	}
+	
+	void Name::set_middle(std::string middle)
+	{
+		format_name(middle);
+		middle_ = middle;
+	}
+	
+	void Name::set_last(std::string last)
+	{
+		format_name(last);
+		last_ = last;
+	}
+
+	std::ostream& operator<<(std::ostream& outs, const Name& n)
+	{
+		// In case middle name given.
+		if (n.middle_ != "")
+		{
+			outs << n.first_ << " " << n.middle_[0] << ". " << n.last_;
+		}
+		else
+		{
+			outs << n.first_ <<" "<< n.last_;
+		}
+		
+		return outs;
+	}
+
+	bool operator == (const Name& n1, const Name& n2)
+	{
+		return n1.first_ == n2.first_ && n1.middle_ == n2.middle_
+			&& n1.last_ == n2.last_;
+	}
+	
+	bool operator != (const Name& n1, const Name& n2)
+	{
+		return !(n1 == n2);
+	}
+
+	// FOR CLASS gift_list
+
+	gift_list::gift_list()
+	{
+		gifts_bag person_gifts;
+		list_ = person_gifts;
+	}
+
+	gift_list::gift_list(const Name& person, const gifts& the_gifts)
+	{
+		gifts_bag person_gifts(person, the_gifts);
+		list_ = person_gifts;
+	}
+
+	void gift_list::add(const Name& person, const gifts& the_gifts)
+	{
+		list_.insert(person, the_gifts);
+	}
+
+	bool gift_list::in_list(const Name& person)const
+	{
+		return list_.is_item(person);
+	}
+
+	void gift_list::add_gift(const Name& person, const gift& the_gift)
+	{
+		assert(in_list(person));
+
+		// If the_gift is already in the list don't add it
+		for (auto a_gift : list_[person])
+			if (a_gift.first == the_gift.first)
+				return;
+		list_[person].push_back(the_gift);
+	}
+
+	gift_list::gifts gift_list::get_gifts(const Name& person)const
+	{
+		assert(in_list(person));
+		return list_[person];
+	}
+
+	double gift_list::gifts_cost(const Name& person)const
+	{
+		assert(in_list(person));
+		double cost = 0;
+		for (auto i : list_[person])
+		{
+			cost += i.second;
+		}
+		return cost;
+	}
+	
+	double gift_list::gifts_cost()const
+	{
+		double cost = 0;
+		// iterate over every person in the list + calculating his/her gift costs
+		// and adding it to the total cost.
+		for (auto person : list_)
+		{
+			cost += gifts_cost(person.first);
+		}
+		return cost;
+	}
+
+	void gift_list::remove(const Name& person)
+	{
+		assert(in_list(person));
+		list_.remove(person);
+	}
+
+	bool gift_list::remove_gift(const Name& person, std::string gift_name)
+	{
+		assert(in_list(person));
+
+		// go over the person's gifts and when the target gift is found, remove it from the vector of gifts
+		for (auto it = list_[person].begin(); it != list_[person].end(); ++it)
+		{
+			if (it->first == gift_name)
+			{
+				list_[person].erase(it);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// for debugging
+	void print_list(const gift_list& the_list)
+	{
+		gift_list::gifts_bag my_list = the_list.get_list();
+		for (auto it = my_list.begin(); it != my_list.end(); ++it)
+		{
+			std::cout << "Name: ";
+			std::cout << (*it).first << "\n";
+			std::cout << "Gifts: ";
+			for (auto i = 0; i < (*it).second.size(); ++i)
+			{
+				std::cout << (*it).second[i].first << "| ";
+			}
+			std::cout<<std::endl;
+			std::cout << "-----------------------\n";
+		}
+	}
+
+} // end of namespace
---
+++ Gift List/Gift List/gift_list.h
@@ -0,0 +1,148 @@
+// This is the header file for the class gift_list and a simple class Name
+// The classes are within namespace (gift_list_adnan)
+// 
+// @Author: Adnan Mohamed
+//
+// CLASS Name:
+//	CONSTRUCTOR:
+//	 Name(std::string first = "", std::string last = "", std::string middle = "")
+//	  Postcondition: if all parameters provided, the name will have first, last, and middle name.
+//	  Note that the last name parameter comes before the middle name.
+//	  DEFAULT ARGUMENTS: the default will be an empty string
+//
+// NON-MEMBER FUNCTIONS FOR CLASS Name:
+//  std::ostream& operator<<(std::ostream&, const Name&);
+//	  Postcondition: outputs the name to where the ostream is connected
+//
+//	bool operator == (const Name&, const Name&);
+//	  Postcondition: returns true if every name (first, middle, and last) of both names are equal
+//	  else returns false.
+//
+//	bool operator != (const Name&, const Name&);
+//	  Postcondition: returns true incase Name == Name2 returns false, else returns false.
+//
+//-----------------------------
+// CLASS gift_list:
+//	TYPEDEFS:
+//  typedef std::pair<std::string, double> gift (the string is for the name of the gift, and double for price $)
+//  typedef std::vector<gift> gifts  (type for list of gifts)
+//  typedef template_keyed_bag_adnan::keyed_bag<Name, gifts> gifts_bag (type for list of people and their list of gifts)
+//	typedef size_t size_type (type for any variable keeping track of size)
+//
+// CONSTRUCTORS
+//  gift_list()
+//	 Postcondition: an empty list is created (no people, no gifts)
+//
+//  gift_list(const Name&, const gifts&)
+//	 Postcondition: a list with one person and his gifts is created
+//
+// MODIFICATION MEMBER FUNCTIONS
+//	void add(const Name&, const gifts&);
+//	 Precondition: Name x is in the list. ( is_list(Name) returns true )
+//	 Postcondition: The list has the given Name and the his/her list of gifts
+//
+//	void add_gift(const Name&, const gift&)
+//	 Precondition: Name x is in the list. ( is_list(Name) returns true )
+//	 Postcondition: The gift is added to the gift list of the person given his/her name
+//
+//	void remove(const Name&)
+//	 Precondition: Name x is in the list. ( is_list(Name) returns true )
+//	 Postcondition: The given name no more exists in the list.
+//	
+//	bool remove_gift(const Name&, string)
+//	 Precondition: Name x is in the list. ( is_list(Name) returns true )
+//	 Postcondition: returns true if the gift is removed from the person's gifts.
+//	 returns false if the gift is not found in the person's gifts.
+//
+// NON-MODIFICATION MEMBER FUNCTIONS:
+//  bool in_list(const Name&)const
+//	 Postcondition: Returns true if there is a person with the specified name in the list, else false
+//
+//	gifts get_gifts(const Name&)const;
+//	 Precondition: Name x is in the list. ( is_list(Name) returns true )
+//	 Postcondition: the list of gifts for the given person will be returned
+//
+//	double gifts_cost(const Name&)const;
+//	 Precondition: Name x is in the list. ( is_list(Name) returns true )
+//	 Postcondition: returns the costs of gifts for the specified person
+//
+//  double gifts_cost()const
+//	 Postcondtion: returns the total cost of gifts
+//
+//  gifts_bag get_list()const
+//	 Postcondition: returns the entire list of people associated with their gifts.
+//
+//  size_type size()const
+//	 Postcondition: returns the number of people this gift_list contains
+
+#ifndef GIFT_LIST_H
+#define GIFT_LIST_H
+#include<string>
+#include<utility>
+#include<vector>
+#include"keyed_bag/template_keyed_bag.h"
+
+namespace gift_list_adnan
+{
+	class Name {
+	public:
+		Name(std::string first = "", std::string last = "", std::string middle = "");
+		
+		// GETTERS
+		std::string first()const { return first_; }
+		std::string middle()const { return middle_; }
+		std::string last()const { return last_; }
+		// SETTERS
+		void set_first(std::string);
+		void set_middle(std::string);
+		void set_last(std::string);
+
+		// OUTPUT OPERATOR
+		friend std::ostream& operator<<(std::ostream&, const Name&);
+		// EQUALITY
+		friend bool operator == (const Name&, const Name&);
+		friend bool operator != (const Name&, const Name&);
+
+	private:
+		std::string first_;
+		std::string middle_;
+		std::string last_;
+		void format_name(std::string&);
+	};
+	
+
+	class gift_list
+	{
+	public:
+		typedef std::pair<std::string, double > gift;
+		typedef std::vector<gift> gifts;
+		typedef template_keyed_bag_adnan::keyed_bag<Name, gifts> gifts_bag;
+		typedef size_t size_type;
+		// CONSTRUCTORS
+		gift_list();
+		gift_list(const Name&, const gifts&);
+
+		// MODIFICATION MEMBER FUNCTIONS
+		void add(const Name&, const gifts&);
+		void add_gift(const Name&, const gift&);
+		void remove(const Name&);
+		bool remove_gift(const Name&, std::string);
+
+		// NON-MODIFICATION MEMBER FUNCTIONS
+		bool in_list(const Name&)const;
+		gifts get_gifts(const Name&)const;
+		double gifts_cost(const Name&)const;
+		double gifts_cost()const;
+		gifts_bag get_list()const { return list_; }
+		size_type size()const { return list_.size(); }
+	private:
+		gifts_bag list_;
+	};
+
+	// FOR DEBUGGING
+	void print_list(const gift_list&);
+}
+
+
+#endif // !GIFT_LIST_H
+
