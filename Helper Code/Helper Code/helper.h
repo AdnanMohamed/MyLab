9f32a4b5fc7b6b1b7edd7bbd10c7200a6d44e789
Adnan Mohamed <amohame6@kent.edu>
2020-03-20 17:32:51 +0300

    added bubble sort template function and it's tested, 3/20/2020 5:32 PM

--- Helper Code/Helper Code/helper.h
+++ Helper Code/Helper Code/helper.h
@@ -16,6 +16,20 @@
 //	 Postcondition: If the element is found, the index of the element which equals 'target'
 //	 will be returned. else -1 will be the return value.
 //
+//	template<class Item>
+//	void bubble_sort(Item arr[], int size, bool Ascend);
+//	 Precondition: Item could be any type which has < and =
+//	 arr[]: array of type Item,
+//	 size: The number of elements in the array
+//	 Ascend: Indicator to whether sorting in ascending order or descending.
+//	 Postcondition: If Ascend is true, the elements will be sorted from smallest
+//	 to largest (relying on < operator), else sorted from largest to smallest.
+//
+//  template<class Item>
+//	void swap(Item& e1, Item& e2)
+//	 Precondition: Item is any type that has the = operator
+//	 Postcondition: e1 and e2 are swapped.
+//
 #ifndef HELPER_CODE
 #define HELPER_CODE
 
@@ -25,6 +39,11 @@ namespace helper_adnan {
 	template<class Item>
 	int binary_search(Item arr[], int size, const Item& target);
 
+	template<class Item>
+	void bubble_sort(Item arr[], int size, bool Ascend = true);
+
+	template<class Item>
+	void swap(Item& e1, Item& e2);
 
 } // end of namespace
 
