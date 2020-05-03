275880943a37ace4475bd16dcce1deac17c3f1af
Adnan Mohamed <amohame6@kent.edu>
2020-05-03 14:40:50 +0300

    erased commented code.

--- Set Class (using b_trees)/set2.h
+++ Set Class (using b_trees)/set2.h
@@ -134,7 +134,7 @@ namespace set2_adnan {
 
     private:
         // MEMBER CONSTANTS
-        static const std::size_t MINIMUM = 3;
+        static const std::size_t MINIMUM = 2;
         static const std::size_t MAXIMUM = 2 * MINIMUM;
         // MEMBER VARIABLES
         std::vector<Item> data;
